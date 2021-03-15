9df7d6b4e942239ac0e357200da60ae966955d05
juanjo141516 <79122498+juanjo141516@users.noreply.github.com>
2021-03-14 23:20:36 -0500

    Eliminación del archivo js

--- index.js
+++
@@ -1,8 +0,0 @@
-var openCloseMenu = () =>  {
-    var _menu = document.getElementById('Menu');
-    if (_menu.style.display === "none") {
-        _menu.style.display = "block";
-    } else {
-        _menu.style.display = "none";
-    }
-}
